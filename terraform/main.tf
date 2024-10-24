5c3bfffe71e8ec8d9bfd1c986aee84319bc89113
Chakradhar-Grandhi <grandhidurga.c@northeastern.edu>
2024-10-24 11:43:20 -0400

    rds instance addition

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,23 +8,31 @@ resource "aws_vpc" "main" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = {
-    Name = "${var.vpc_name}-${var.unique_suffix}"
-  }
+  tags = merge(var.tags, { Name = "${var.vpc_name}-${var.unique_suffix}" })
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "igw-${var.unique_suffix}"
-  }
+  tags = merge(var.tags, { Name = "igw-${var.unique_suffix}" })
 }
 
 resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_cidr
+  cidr_block              = var.public_subnet_cidrs[count.index]
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${var.unique_suffix}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "public-subnet-${var.unique_suffix}"
@@ -39,14 +47,25 @@ resource "aws_route_table" "public" {
     gateway_id = aws_internet_gateway.main.id
   }
 
-  tags = {
-    Name = "public-rt-${var.unique_suffix}"
-  }
+  tags = merge(var.tags, { Name = "public-rt-${var.unique_suffix}" })
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, { Name = "private-rt-${var.unique_suffix}" })
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_security_group" "application_sg" {
@@ -89,15 +108,37 @@ resource "aws_security_group" "application_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    Name = "application_security_group"
+  tags = merge(var.tags, { Name = "application-security-group-${var.unique_suffix}" })
+}
+
+resource "aws_security_group" "database_sg" {
+  name = "database_sg"
+  description = "Database security group for RDS instance"
+  vpc_id = aws_vpc.main.id
+
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    security_groups = [aws_security_group.application_sg.id]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.tags, { Name = "database-security-group-${var.unique_suffix}" })
 }
 
 resource "aws_instance" "app_instance" {
+  count                       = length(var.public_subnet_cidrs)
   ami                         = var.custom_ami
   instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.public.id
+  subnet_id                   = aws_subnet.public[count.index].id
   vpc_security_group_ids      = [aws_security_group.application_sg.id]
   associate_public_ip_address = true
   key_name                    = var.key_pair_name
@@ -112,15 +153,53 @@ resource "aws_instance" "app_instance" {
     prevent_destroy = false # No accidental termination protection
   }
   user_data = <<-EOF
-              #!/bin/bash
-              if which git; then
-                echo "Git is installed!"
-              else
-                echo "Git is not installed, as required."
-              fi
-            EOF
+    #!/bin/bash
+    echo "DB_USERNAME=${var.db_username}" >> /etc/environment
+    echo "DB_PASSWORD=${var.db_password}" >> /etc/environment
+    echo "DB_HOSTNAME=${aws_db_instance.rds_instance.address}" >> /etc/environment
+  EOF
 
-  tags = {
-    Name = "web_app_instance"
+  tags = merge(var.tags, { Name = "web-app-instance-${var.unique_suffix}" })
+}
+
+resource "aws_db_parameter_group" "mydb_parameter_group" {
+  name   = "rds-webapp-db-parameter-group"
+  family = var.db_family
+
+  parameter {
+    name  = "max_connections"
+    value = "100"
   }
+
+  tags = merge(var.tags, { Name = "mydb-parameter-group-${var.unique_suffix}" })
+}
+
+# RDS Subnet Group for Private Subnet
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group-${var.unique_suffix}"
+  subnet_ids = aws_subnet.private[*].id
+
+  tags = merge(var.tags, { Name = "rds-subnet-group-${var.unique_suffix}" })
+}
+
+
+
+
+resource "aws_db_instance" "rds_instance" {
+  allocated_storage    = 10
+  instance_class       = var.db_instance_class
+  engine               = var.db_engine
+  engine_version       = var.db_engine_version
+  identifier           = var.db_identifier
+  username             = var.db_username
+  password             = var.db_password
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+  parameter_group_name = aws_db_parameter_group.mydb_parameter_group.name
+  vpc_security_group_ids = [aws_security_group.database_sg.id]
+  multi_az             = false
+  publicly_accessible  = false
+  skip_final_snapshot  = true
+  db_name              = var.db_name
+
+  tags = merge(var.tags, { Name = "rds-instance-${var.unique_suffix}" })
 }
